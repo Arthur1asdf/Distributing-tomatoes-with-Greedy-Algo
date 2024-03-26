d116bdf60356d0fa46baaa78492ff68886657d8f
Arthur1asdf <arthurteng0819@gmail.com>
2024-03-25 21:33:58 -0400

    finished

--- src/App.java
+++ src/App.java
@@ -1,7 +1,3 @@
 public class App {
-    public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        System.out.println("I do not like minors");
-        System.out.println("Ima bouta kammmm");
-    }
+  
 }
